ead14489638f0792409d3f3eafbb17da0b6b8e2e
phgrosjean <phgrosjean@sciviews.org>
2018-04-23 11:48:43 +0200

    Typo in the description

--- vignettes/svGUI.R
+++ vignettes/svGUI.R
@@ -11,3 +11,13 @@ library(svGUI)
 ## ------------------------------------------------------------------------
 names(.GUI)
 
+## ------------------------------------------------------------------------
+gui_ask(.GUI)
+# Turn the ask property off, so that the GUI does not interrupt R any more
+gui_ask(.GUI) <- FALSE
+gui_ask(.GUI)
+
+## ------------------------------------------------------------------------
+gui_add("tk_gui", widgets = "tcltk", ask = TRUE)
+gui_list()
+
